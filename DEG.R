5f40c96a082da4bd507cf6486d204f129f544bcb
Sigit <chansigit@users.noreply.github.com>
2019-06-19 19:41:00 +0800

    Update DEG.R

--- DEG.R
+++ DEG.R
@@ -3,8 +3,7 @@ my_DE <-function(object, identity="RNA_snn_res.0.5", name="sample",
                  min.pct=0.25, 
                  logfc.threshold=0.25, 
                  pval.thres=0.01, 
-                 adjpval.thres=0.005,...
-                  ){
+                 adjpval.thres=0.005,...){
   # 0. setting clustering information and do DEG analysis
   Idents(object)<-identity
   tic("Perform differential expression analysis")
@@ -23,16 +22,19 @@ my_DE <-function(object, identity="RNA_snn_res.0.5", name="sample",
                              "/MarkerList.", name, ".", identity, "_", 
                              "all.csv",      sep = "")
   write.csv(markers.sig,    file = fp_allmarkerlist)
+  print(paste("Write to ",fp_allmarkerlist))
 
   fp_posmarkerlist <- paste0(outpath.prefix, 
                              "/MarkerList.", name, ".", identity, "_", 
                              "pos.csv",      sep = "")
   write.csv(markers.sigpos, file = fp_posmarkerlist)
+  print(paste("Write to ",fp_posmarkerlist))
 
   fp_negmarkerlist <- paste0(outpath.prefix, 
                              "/MarkerList.", name, ".", identity, "_", 
                              "neg.csv",      sep = "")
   write.csv(markers.signeg, file = fp_negmarkerlist)
+  print(paste("Write to ",fp_negmarkerlist))
 
   # 3. prepare a list object to return
   DE.result                       <- list()  
@@ -45,8 +47,7 @@ my_DE <-function(object, identity="RNA_snn_res.0.5", name="sample",
 
 my_DEplot <- function(object, DE.result, w, h, outpath.prefix,
                       identity="RNA_snn_res.0.5", name="sample",
-                      given.identity.order=NULL, topNGenes=40L, 
-                      min.pct=0.25, logfc.threshold=0.25, pval.thres=0.01, adjpval.thres=0.005,...){
+                      given.identity.order=NULL, topNGenes=40L,...){
   DE.result[["all_markers"]]      -> markers.sig
   DE.result[["positive_markers"]] -> markers.sigpos
   DE.result[["negative_markers"]] -> markers.signeg
@@ -68,27 +69,33 @@ my_DEplot <- function(object, DE.result, w, h, outpath.prefix,
   fp <- paste0(outpath.prefix,
                "/Heatmap.", name, ".", identity, "_", 
                "allMarkers.pdf",  sep = "")
+  tic("rendering")
   pdf(fp, width=w, height=h)
-  my_heatmap(object, genes=markers.sig.top, given.identity.order=given.identity.order, 
+  my_heatmap(object, genes=markers.sig.top$gene, given.identity.order=given.identity.order, 
              annotation_height=2, group.by = identity)
   dev.off()
+  toc()
 
   # ---------- Draw positive markers on heatmap ----------
   fp <- paste0(outpath.prefix,
                "/Heatmap.", name, ".", identity, "_", 
                "posMarkers.pdf",  sep = "")
+  tic("rendering")
   pdf(fp, width=w, height=h)
-  my_heatmap(object, genes=markers.sigpos.top, given.identity.order=given.identity.order, 
+  my_heatmap(object, genes=markers.sigpos.top$gene, given.identity.order=given.identity.order, 
              annotation_height=2, group.by = identity)
   dev.off()
+  toc()
 
   # ---------- Draw negative markers on heatmap ----------
   fp <- paste0(outpath.prefix,
                "/Heatmap.", name, ".", identity, "_", 
                "negMarkers.pdf",  sep = "")
+  tic("rendering")
   pdf(fp, width=w, height=h)
-  my_heatmap(object, genes=markers.signeg.top, given.identity.order=given.identity.order, 
+  my_heatmap(object, genes=markers.signeg.top$gene, given.identity.order=given.identity.order, 
              annotation_height=2, group.by = identity)
   dev.off()
+  toc()
 
 }
