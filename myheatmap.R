c2b9a468254230d397e3b213a0e10234aea2a69b
Sigit <chansigit@users.noreply.github.com>
2019-07-26 16:00:02 +0800

    Update myheatmap.R

--- myheatmap.R
+++ myheatmap.R
@@ -1,16 +1,20 @@
 ###############################################################################
 #
 # Author:  Sijie Chen    
-# Version: 1.0   
+# Version: 1.0.1
 # Time:    2019/07/26
 #
 ###############################################################################
-myheatmap<-function(object, genes, slot="scale.data", annotation_height=1,
-                     group.by="RNA_snn_res.0.5", given.identity.order=NULL, 
-                     palette=c('#a6cee3','#1f78b4','#b2df8a','#33a02c',
-                               '#fb9a99','#e31a1c','#fdbf6f','#ff7f00',
-                               '#cab2d6','#6a3d9a','#ffff99','#b15928',
-                               '#980043','#02818a','#00441b','#984ea3'),...){
+my.seurat.heatmap<-function(object, genes, slot="scale.data", 
+                     group.by="RNA_snn_res.0.5", 
+                     given.identity.order=NULL,
+                     annotation_height=1, 
+                     identity.palette=c('#a6cee3','#1f78b4','#b2df8a','#33a02c',
+                                        '#fb9a99','#e31a1c','#fdbf6f','#ff7f00',
+                                        '#cab2d6','#6a3d9a','#ffff99','#b15928',
+                                        '#980043','#02818a','#00441b','#984ea3'),
+                     hm.color.lower="#FF08BD", hm.color.mid="#000000", hm.color.upper="#FFF42F",
+                     hm.color.lb=-1.5, hm.color.ub=1.5, ...){
     library(ComplexHeatmap)
     # ==========================================================================
     # Filter undetected genes out
