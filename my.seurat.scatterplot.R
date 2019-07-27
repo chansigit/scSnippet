c4054efd741273283beb485c525a38977936e570
Sigit <chansigit@users.noreply.github.com>
2019-07-27 21:37:29 +0800

    Update and rename scatter.R to my.seurat.scatterplot.R

--- my.seurat.scatterplot.R
+++ my.seurat.scatterplot.R
@@ -1,5 +1,8 @@
-my_scatter_plot<- function(object, group.by, reduction="umap",contour=FALSE, pt.size=0.1, label.pt.size=5,
-                           label.size=10, label.face="plain",label.rectangle=FALSE, ...){   
+my.seurat.scatterplot<- function(object, group.by, reduction="umap",contour=FALSE, pt.size=0.1, label.pt.size=5,
+                           label.size=10, label.face="plain",label.rectangle=FALSE, palette=c('#a6cee3','#1f78b4','#b2df8a','#33a02c',
+                '#fb9a99','#e31a1c','#fdbf6f','#ff7f00',
+                '#cab2d6','#6a3d9a','#00441b','#b15928',
+                '#980043','#02818a','#984ea3','#ffff99'),...){   
   # 1. bind dimensionality reduction info with metainfo
   meta_dr <- cbind( object[[]], Embeddings(object = object, reduction = reduction))
   
@@ -16,13 +19,10 @@ my_scatter_plot<- function(object, group.by, reduction="umap",contour=FALSE, pt.
     xcoord.name<-"PC_1";  ycoord.name<-"PC_2"
   }
     
-    
   # 3. bind barcode info stored in rownames in case of getting lost when rownames were discarded
   meta_dr <- cbind("barcode"=rownames(meta_dr), meta_dr) 
   meta_dr <- meta_dr[order(meta_dr[,group.by], meta_dr[,xcoord.name], meta_dr[,ycoord.name]) , ]
   
-  
-
   # 4. add text labels for each group
   for (identity in levels(as.factor(meta_dr[,group.by]))){
     # select ordered dataframe by groups
@@ -42,10 +42,7 @@ my_scatter_plot<- function(object, group.by, reduction="umap",contour=FALSE, pt.
     ellipse = (contour==TRUE) , 
     label = "text_annotation",repel=TRUE,font.label = c(label.size, label.face, "black"),label.rectangle=label.rectangle,
     color = group.by, 
-    palette = c('#a6cee3','#1f78b4','#b2df8a','#33a02c',
-                '#fb9a99','#e31a1c','#fdbf6f','#ff7f00',
-                '#cab2d6','#6a3d9a','#ffff99','#b15928',
-                '#980043','#02818a','#00441b','#984ea3') )  + 
+    palette = palette )  + 
   guides(colour = guide_legend(override.aes = list(size=label.pt.size)))+ # legend dot size
   theme(legend.position = "bottom", # legend themes
         legend.title    = element_text(color = "blue", size = 10),
@@ -53,5 +50,3 @@ my_scatter_plot<- function(object, group.by, reduction="umap",contour=FALSE, pt.
   
   plot
 }
-options(repr.plot.width=15,repr.plot.height=10,repr.plot.resolution=500)
-my_scatter_plot(ASI, group.by="rep.ident", reduction="umap",pt.size=0.1,contour=F)
