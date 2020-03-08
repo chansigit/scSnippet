a407369415f48ac995105174d4e0e1b4ea23ffa4
Sigit <chansigit@users.noreply.github.com>
2020-03-08 22:43:03 +0800

    Update CellCycle.R

--- Cell_Cycle_Annotation/CellCycle.R
+++ Cell_Cycle_Annotation/CellCycle.R
@@ -1,7 +1,7 @@
 # mouse  mm.s.genes  mm.g2m.genes
 load(url("https://raw.githubusercontent.com/chansigit/scSnippet/master/Cell_Cycle_Annotation/mm.g2m.rda"))
 load(url("https://raw.githubusercontent.com/chansigit/scSnippet/master/Cell_Cycle_Annotation/mm.s.rda"))
-CellCycleScoring(seu, s.features=mm.s, g2m.features=mm.g2m)
+seu<-CellCycleScoring(seu, s.features=mm.s, g2m.features=mm.g2m)
 
 # human
-CellCycleScoring(seu, s.features=Seurat::cc.genes$s.genes, g2m.features=Seurat::cc.genes$g2m.genes)
+seu<-CellCycleScoring(seu, s.features=Seurat::cc.genes$s.genes, g2m.features=Seurat::cc.genes$g2m.genes)
