15987a0cb6759bdcd9dead5ce957c4a19f2ae2a8
sijie <chansigit@gmail.com>
2020-03-31 21:37:18 +0800

    human cc genes

--- Cell_Cycle_Annotation/PythonCCGeneList.py
+++ Cell_Cycle_Annotation/PythonCCGeneList.py
@@ -4,3 +4,10 @@ mm_g2m_genes = [eval(str.strip(str(line, encoding = "utf-8"))) for line in respo
 
 response = urllib.request.urlopen("https://raw.githubusercontent.com/chansigit/scSnippet/master/Cell_Cycle_Annotation/mm.s.tsv")
 mm_s_genes = [eval(str.strip(str(line, encoding = "utf-8"))) for line in response]
+
+
+
+cell_cycle_genes = [x.strip() for x in open('./regev_lab_cell_cycle_genes.txt')]
+s_genes = cell_cycle_genes[:43]
+g2m_genes = cell_cycle_genes[43:]
+#cell_cycle_genes = [x for x in cell_cycle_genes if x in adata.var_names]
