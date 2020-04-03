3e4afa4f6777ba94c40b3874d2d6e8e32d742996
Sigit <chansigit@users.noreply.github.com>
2020-04-03 16:02:49 +0800

    Update and rename Seurat2Scanpy.R to Seurat2Scanpy.Rpy

--- Seurat2Scanpy.R
+++
@@ -1,8 +0,0 @@
-tic()
-dge <- t(as.matrix(GetAssayData(seu, assay = "RNA", slot = "counts")))
-var <- colnames(dge)
-obs <- seu@meta.data
-write.table(dge, sep = "\t", row.names =T, col.names = T, file="./*_dge.tsv")
-write.table(var, sep = "\t" ,row.names =F, col.names = F, file="./*_var.tsv")
-write.table(obs, sep = "\t", row.names =T, col.names = T, file="./*_obs.tsv")
-toc()
