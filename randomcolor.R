40b204cd8fc14b0bacf675579dfb0f7e29b1775a
Sigit <chansigit@users.noreply.github.com>
2021-04-07 14:06:29 +0800

    Update randomcolor.R

--- randomcolor.R
+++ randomcolor.R
@@ -4,3 +4,9 @@ qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',] # one of 'd
 col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 col_vector=sample(col_vector)
 pie(rep(1,n), col=sample(col_vector, n))
+
+
+plot(seq_len(length(col_vector)), 
+     rep_len(1, length(col_vector)),
+     col = col_vector, 
+     pch = 16, cex =5, xaxt = 'n', yaxt = 'n', xlab = '', ylab = '')
