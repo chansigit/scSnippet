fbd08bd33b899a099f3a6f707dec7bb1d4fecdfb
Sigit <chansigit@users.noreply.github.com>
2021-04-13 16:59:19 +0800

    Create cytokine_list.R

---
+++ cytokine_list.R
@@ -0,0 +1,46 @@
+#chemokine<-c("Ccl27a", "Gm13306", "Ccl21d", "Gm13304", "Ccl19", 
+#             "Cxcl14", "Ccl21b", "Gm10591", "Ccl21a", "Ccl27b", 
+#             "Cxcl17", "Cxcl5", "Ccl9", "Ccl6", "Ccl4", "Ccl3", 
+#             "Ccl22", "Ccl17", "Xcl1", "Ccl7", "Cx3cl1", "Cxcl12", 
+#             "Cxcl13", "Ccl11", "Cxcl9", "Ccl2", "Ccl28", "Cxcl16", 
+#             "Cxcl11", "Ccl12", "Ccl8", "Ccl20", "Cxcl10", "Ccl24",
+#             "Cxcl1","Cxcl2","Cxcl6","Cxcl8")
+
+chemokine<-c("Ccl1","Ccl2","Ccl3","Ccl4","Ccl5","Ccl6","Ccl7","Ccl8",
+"Ccl9","Ccl11","Ccl12","Ccl17","Ccl19","Ccl20","Ccl21a","Ccl21b",
+"Ccl21c","Ccl21d","Ccl22","Ccl24","Ccl25","Ccl26","Ccl27a","Ccl27b",
+"Ccl28","Gm13304","Gm10591","Gm13306","Gm13309","Gm21104","Gm2457","Gm2564","Enc1",
+
+"Cxcl1","Cxcl2","Cxcl3","Pf4","Cxcl5",
+"Ppbp","Cxcl9","Cxcl10","Cxcl12","Cxcl13","Cxcl14",
+"Cxcl15","Cxcl16","Cxcl17",
+
+"Cx3cl1", "Xcl1"
+)
+
+#il<-c("Il7", "Il17a", "Il17f", "Il2", "Il11", "Il21", "Il5", "Il9", "Il1f5",
+#    "Il25", "Il12a", "Il13", "Il1f9", "Il1f10", "Il4", "Il3", "Il33", 
+#    "Il12b", "Il1f6", "Il6", "Il1rn", "Il17b", "Il1f8", "Il20", "Il31",
+#    "Il16", "Il10", "Il23a", "Il24", "Il17c", "Il18", "Il34", "Il22", 
+#    "Iltifb", "Il19", "Il17d", "Il15", "Il1b", "Il1a", "Il27")
+
+il<-c( "Il1a", "Il1b", "Il2", "Il3", "Il4", "Il5", "Il6", "Il7", "Il9", 
+ "Il10", "Il11", "Il12a", "Il12b", "Il13", "Il15", "Il16", 
+ "Il17a", "Il17b", "Il17c", "Il17d", "Il17f", "Il18", "Il19", 
+ "Il20", "Il21", "Il22", "Il22b", "Iltifb", "Il23a", "Il24", 
+ "Il25", "Il27", "Il31", "Il33", "Il34", "Il36a", "Il36b", "Il36g", 
+ "Il1f5", "Il1f6", "Il1f8", "Il1f9", "Il1f10", "Il1rn")
+
+ifn<-c("Ifnk", "Ifnl2", "Ifnl3", "Il6", "Ifna15", "Ifna14", "Ifna9", 
+       "Ifna12", "Ifna13", "Ifna16", "Ifna2", "Ifnab", "Ifna7", "Ifna11", 
+       "Ifna6", "Ifna5", "Ifna4", "Ifna1", "Ifng", "Ifnb1")
+
+tnf<-c("Tnfsf13b", "Cd40lg", "Tnfsf10", "Eda", "Tnfsf15", "Tnfsf8", "Tnf",
+       "Tnfsf13", "Tnfsf4", "Tnfsf12", "Tnfsf9", "Ltb", "Tnfsf18", 
+       "Fasl", "Tnfsf11", "Lta", "Cd70", "Tnfsf14")
+
+others<- c("Csf1", #M-CSF
+           "Csf2", #Gm-CSF
+           "Csf3", #G-CSF
+           "Epo", # erythropoietin
+           "Tgfb1","Tgfb2","Tgfb3")
