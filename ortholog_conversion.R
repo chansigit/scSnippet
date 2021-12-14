5815b6d02ae5b5a9deaea16238e03b7b9e025e94
Sigit <chansigit@users.noreply.github.com>
2021-12-14 17:49:11 +0800

    Update ortholog_conversion.R

--- ortholog_conversion.R
+++ ortholog_conversion.R
@@ -7,3 +7,11 @@ rat   = useMart("ensembl", dataset = "rnorvegicus_gene_ensembl")
 genesV2 = getLDS(attributes = c("mgi_symbol"), filters = "mgi_symbol", 
                  values = x , mart = mouse, 
                  attributesL = c("rgd_symbol"), martL = rat, uniqueRows=T)
+
+
+# query what databases are available 
+mart = useMart('ensembl')
+listDatasets(mart) 
+
+# query what columns are available
+listAttributes(mart = rat)
